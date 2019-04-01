15c297e131e6648dc2e1e2731ed085927cc70a69
Nate Tan <nktan93@gmail.com>
2019-04-01 21:15:46 +0000

    Automatically backed up by Learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -15,8 +15,8 @@ class String
   end
 
   def count_sentences
-    self.split!('.')
-    self.split!('!')
-    self.split!('?')
+    self.split('.')
+    self.split('!')
+    self.split('?')
   end
 end
