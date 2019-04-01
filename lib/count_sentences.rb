5e07b25df23d6944983c595b68f00f207e5f4dec
Nate Tan <nktan93@gmail.com>
2019-04-01 21:33:45 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,20 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split_sentence = self.split(/[.?!]/)
+    split_sentence.delete("")
+    split_sentence.length
   end
 end
